3e771f9239e0e069296e613c317b07329de337cf
Adrian Klimek <git@adrianklimek.com>
2023-11-19 14:01:20 +0100

    chore: Set up tooling

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: false,
+  arrowParens: 'avoid',
+  trailingComma: 'none',
+  singleQuote: true,
+  endOfLine: 'lf',
+  bracketSpacing: true,
+  printWidth: 80,
+  useTabs: false,
+  quoteProps: 'as-needed',
+  tabWidth: 2
+}
+
+export default config
